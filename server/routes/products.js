a8324764eb26d4efc5efdd5e8dba69135539f68b
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2016-11-23 13:58:22 -0600

    added products router

---
+++ server/routes/products.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/sigma';
+
+router.get('/', function(req, res) {
+  console.log('reached get route!')
+  // get customers from DB
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT description, unit_price, SUM(on_hand) AS on_hand FROM products JOIN warehouse_product ON products.id = warehouse_product.product_id GROUP BY description, unit_price;', function(err, result) {
+      done(); // close the connection.
+
+      if(err) {
+        console.log('select query error: ', err);
+        res.sendStatus(500);
+      }
+      console.log(result.rows);
+      res.send(result.rows);
+
+    });
+
+  });
+});
+
+module.exports = router;
