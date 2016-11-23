c02b56504abcff0f52927029887f393a21baf9c4
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-23 13:23:21 -0600

    search working

--- server/routes/warehouse.js
+++ server/routes/warehouse.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res) {
       res.sendStatus(500);
     }
 
-    client.query('SELECT * FROM warehouse', function(err, result) {
+    client.query('SELECT warehouse, fulfillment_days, description FROM warehouse JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id JOIN products ON products.id = warehouse_product.product_id;', function(err, result) {
       done();
 
       if(err) {
